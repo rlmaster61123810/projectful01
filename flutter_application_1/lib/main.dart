2286dbb532e2bfd4c22d4b744aecaf4807b950a2
Nontawat Wuttifaey <mistidea9@gmail.com>
2021-03-06 15:13:20 +0700

    update

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -5,32 +5,33 @@ void main() {
     MaterialApp(
       home: Scaffold(
         drawer: Drawer(
-          child: Text("hello Drawer")
+          child: Text("Hello Drawer"),
         ),
-    appBar: AppBar(
-      title: Text(
-        "My Fist Project",
+        appBar: AppBar(
+          title: Text(
+            "My First Project",
+          ),
+        ),
+        body: Center(
+          child: Text("hello world"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.phone),
+              label: "Phone"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_customize),
+              label: "Dashboard"
+            ),
+          ],
+        ),
       ),
     ),
-    body: Center(
-      child: Text("hello world",
-    )
-   ),
-   bottomNavigationBar: BottomNavigationBar(items: [
-     BottomNavigationBarItem(
-       icon: Icon(Icons.home),
-       label:"Home"
-       ),
-     BottomNavigationBarItem(
-       icon: Icon(Icons.phone),
-       label:"Phone"
-       ),
-     BottomNavigationBarItem(
-       icon: Icon(Icons.dashboard_customize),
-       label:"Dashboard"
-       ),
-   ],
-   ),
-  ),
-));
+  );
 }
