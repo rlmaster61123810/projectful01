45923facdfc9500a90aacf3c5e5e46404fee0cf9
Nontawat Wuttifaey <mistidea9@gmail.com>
2021-03-07 13:14:14 +0700

    g

--- flutter_application_1/lib/screen/example_screen.dart
+++ flutter_application_1/lib/screen/example_screen.dart
@@ -11,21 +11,111 @@ class _ExampleScreenState extends State<ExampleScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Scaffold(
-         drawer: Drawer(),
-         appBar: AppBar(
-           title: Text(
-             "Pill Reminder",
-           ),
-         ),
-         body: Center(
-           child: Row(
-             children: [
-               Container()
-             ]
-           )
-         )
-       ),
+      child: Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          title: Text(
+            "Example Screen",
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Medicines"),
+                    PopupMenuButton(
+                      itemBuilder: (BuildContext context) {
+                        return [
+                          PopupMenuItem(
+                            child: Text("Wifi"),
+                          ),
+                          PopupMenuItem(
+                            child: Text("Bluetooth"),
+                          )
+                        ];
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      Container(
+                        width: 150,
+                        height: 150,
+                        color: Colors.red,
+                        child: Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.medical_services),
+                              onPressed: () {},
+                            ),
+                            Text("Morning"),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        height: 150,
+                        color: Colors.green,
+                        child: Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.medical_services),
+                              onPressed: () {},
+                            ),
+                            
+                            Text("Afternoon"),
+                          ],
+                        ),
+                      ),
+                    ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      Container(
+                        width: 150,
+                        height: 150,
+                        color: Colors.blue,
+                        child: Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.medical_services),
+                              onPressed: () {},
+                            ),
+                            Text("Everning"),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        height: 150,
+                        color: Colors.orange,
+                        child: Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.medical_services),
+                              onPressed: () {},
+                            ),
+                            Text("Night"),
+                          ],
+                        ),
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
